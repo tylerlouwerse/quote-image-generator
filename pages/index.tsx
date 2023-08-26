7b8c2a5347729298a9498cfa0721d0a99e368f96
tylerlouwerse <tyler@ezmech.ca>
2023-08-26 13:56:57 -0400

    add quote generator styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements';
+
+// Components
+import {
+  BackgroundImage1,
+  GradientBackgroundCon,
+  FooterCon,
+  FooterLink,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorTitle,
+  QuoteGeneratorSubTitle,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+} from '@/components/QuoteGenerator/QuoteGeneratorElements';
+
+// Assets
+import Clouds1 from '../assets/cloud-and-thunder.png';
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -14,7 +33,42 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
-        
+
+        {/* Quote Generator Modal */}
+        {/* <QuoteGeneratorModal   
+        /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a qupte with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+            </QuoteGeneratorSubTitle>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={() => {}}>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        <BackgroundImage1
+          src={Clouds1}
+          height="300"
+          alt="background-image"
+        />
+
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with ♥ by <FooterLink href="https://tylerlouwerse.com" target="_blank" rel="noopener noreferrer">Tyler L</FooterLink>
+          </>
+        </FooterCon>
+
       </GradientBackgroundCon>
     </>
   )

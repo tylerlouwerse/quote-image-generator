bd47d3476ad5cc43a15355503ee180cff164b158
tylerlouwerse <tyler@ezmech.ca>
2023-08-26 13:06:09 -0400

    add gradient background container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements';
 
 export default function Home() {
   return (
@@ -11,6 +12,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.icon" />
       </Head>
+      {/* Background */}
+      <GradientBackgroundCon>
+        
+      </GradientBackgroundCon>
     </>
   )
 }
